972c59fdefda6d1eb1f16cd563cb78107e88a7c3
Anup Chitrakar <chitrakar.anup@gmail.com>
2016-01-01 15:57:15 -0800

    Merge Sort implemented.

--- src/chapter2/sorting/MergeSort.java
+++ src/chapter2/sorting/MergeSort.java
@@ -24,17 +24,49 @@ package chapter2.sorting;
  *
  */
 public class MergeSort {
-	private static int INFINITY = -999999999;
+	private static int INFINITY = 999999999;
 
 	public static void main(String[] args) {
-		int data[] = { 2, 4, 5, 7, 1, 2, 3, 6 };
-		System.out.println("Before Sorting...");
+		int data[] = { 3, 8, 7, 5, 2, 1, 9, 6 };
+		System.out.println("Before Merge Sort...");
 		printArray(data);
-		merge(data, 0, 3, 7);
-		System.out.println("\nAfter Sorting...");
+		mergeSort(data, 0, data.length - 1);
+		System.out.println("\nAfter Merge Sort...");
 		printArray(data);
 	}
 
+	/**
+	 * 
+	 * @param data
+	 *            - unsorted integer array
+	 * @param startIndex
+	 *            - start index of an array
+	 * @param endIndex
+	 *            - end index of an array
+	 */
+	private static void mergeSort(int[] data, int startIndex, int endIndex) {
+		if (startIndex < endIndex) {
+			int mid = (startIndex + endIndex) / 2;
+			mergeSort(data, startIndex, mid);
+			mergeSort(data, mid + 1, endIndex);
+			merge(data, startIndex, mid, endIndex);
+		}
+	}
+
+	/**
+	 * <b>Merge procedure</b> <br/>
+	 * - Generates two sub arrays - left and right. <br/>
+	 * - Compares values in each subarray and sorts. <br/>
+	 * 
+	 * @param data
+	 *            - unsorted integer array
+	 * @param p
+	 *            - begining of an array
+	 * @param q
+	 *            - end of left sub-array
+	 * @param r
+	 *            - end of the an array
+	 */
 	private static void merge(int[] data, int p, int q, int r) {
 		int i = 0, j = 0;
 
@@ -58,7 +90,7 @@ public class MergeSort {
 		i = 0;
 		j = 0;
 
-		for (int k = p; k < r; k++) {
+		for (int k = p; k <= r; k++) {
 			if (left[i] <= right[j]) {
 				data[k] = left[i];
 				i++;
@@ -67,7 +99,6 @@ public class MergeSort {
 				j++;
 			}
 		}
-
 	}
 
 	private static void printArray(int[] data) {
