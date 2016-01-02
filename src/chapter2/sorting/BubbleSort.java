5fc51c32edc5b798034dbe2c8514f11e1551ff6f
Anup Chitrakar <chitrakar.anup@gmail.com>
2016-01-01 16:28:46 -0800

    Bubble Sort Implemented.

---
+++ src/chapter2/sorting/BubbleSort.java
@@ -0,0 +1,37 @@
+package chapter2.sorting;
+
+/**
+ * <b>Bubble Sort</b> <br/>
+ * 
+ * @author achitrakar
+ *
+ */
+public class BubbleSort {
+	public static void main(String[] args) {
+		int data[] = { 3, 41, 52, 26, 38, 57, 9, 49 };
+		System.out.println("Before Bubble Sort...");
+		printArray(data);
+		bubbleSort(data);
+		System.out.println("\nAfter Bubble Sort...");
+		printArray(data);
+	}
+
+	private static void bubbleSort(int[] data) {
+		int temp;
+		for (int i = 0; i < data.length - 2; i++) {
+			for (int j = data.length - 1; j > 0; j--) {
+				if (data[j] < data[j - 1]) {
+					temp = data[j];
+					data[j] = data[j - 1];
+					data[j - 1] = temp;
+				}
+			}
+		}
+	}
+
+	private static void printArray(int[] data) {
+		for (int i : data) {
+			System.out.print(i + " ");
+		}
+	}
+}
